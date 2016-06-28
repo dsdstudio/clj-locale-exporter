02e3925c983447818d7d1e87bc27b101d8a85317
보롱킴 <dsdgun@gmail.com>
2016-06-28 23:59:09 +0900

    apply cider plugin

--- project.clj
+++ project.clj
@@ -7,6 +7,7 @@
                  [com.google.gdata/core "1.47.1"]
                  [org.clojure/data.json "0.2.6"]
                  [google-apps-clj "0.5.1"]]
+  :plugins [[cider/cider-nrepl "0.12.0"]]
   :main ^:skip-aot locale-exporter.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
